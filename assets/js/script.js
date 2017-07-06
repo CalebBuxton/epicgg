1a986ed7888012968f9b098b6539759271609773
Caleb Buxton <TACMB@int.mcmaster.com>
2017-07-06 08:21:09 -0400

    merged calendar and map into main content. fixed position on the map and set size boundaries to fit the content properly. Map now sits in center of the panel and calendar fills panel.

--- assets/js/script.js
+++ assets/js/script.js
@@ -167,15 +167,45 @@ function topVideos() {
 
 
 
-//startup function
+//startup function and eSports Content function
 function pageLoad() {
+	$("#topGames ul").html(" ")
 	$("#firstPanel").text("Current Top Streams");
 	$("#secondPanel").text("Current Top Gaming Videos");
 	topStreams();
 	topGames();
 	topVideos();
+
 }
 
+function switchContent() {
+	$('#firstContentRow').html(" ");
+	$('#secondContentRow').html(" ");
+	$("#firstPanel").text("Upcoming Event Locations");
+	$("#secondPanel").text("Calendar of Events");
+	if ($('#switch').text() === 'View eSports Events') {
+		esportsEvents();
+	}
+
+	else {
+		$("#topGames ul").html(" ")
+		$("#switch").text("View eSports Events")
+		pageLoad();
+	}
+}
+
+function esportsEvents() {
+	$('#firstContentRow').html("<div id='map'></div>");
+	$('#secondContentRow').html("<div id='calendar'></div>");
+	$('#switch').text('View eSports Content');
+	console.log('working');
+	mapSetup();
+}
+
+
+$('#switch').on("click", function(){
+	switchContent();
+})
 
 //Sidebar search interations and email input setup
 $("input[type='text']").keypress(function(event){
@@ -289,6 +319,170 @@ $(document).on('mouseleave','.previewGrid',(function(e){
 	$(this).find("img").removeClass("hidden")
 }));
 
+
+function mapSetup(){
+	mapboxgl.accessToken = 'pk.eyJ1IjoiZGdhYmFpIiwiYSI6ImNqNDNweGx1dDA3NG0zM243MGQzNzQ0bWcifQ.8Auku3GqYC_xjQFCYBtEMg';
+	var map = new mapboxgl.Map({
+    container: 'map', // container id
+    style: 'mapbox://styles/mapbox/streets-v9',
+    center: [-101.54, 39.58], // starting position
+    zoom: 3 // starting zoom
+
+});
+
+	// Add zoom and rotation controls to the map.
+	map.addControl(new mapboxgl.NavigationControl());
+
+	var cities = [
+	{
+		"name": "Atlanta GA",
+		"lat": 33.775,
+		"lng": -84.382,
+		"description": "Terminus Conferece, " + "June 22nd - 25th, " + "https://terminusevent.com/"
+	},
+	{
+		"name": "Minneapolis MN",
+		"lat": 44.978,
+		"lng":  -93.269,
+		"description": "2D Con, " + "August 11 - 13th, " + "https://www.2dcon.net/"
+	},
+	{
+		"name": "Indianapolis IN",
+		"lat": 39.768,
+		"lng": -86.158,
+		"description": "Gen Con 50, " + "August 17 - 20th, " + "http://www.gencon.com/"
+	},
+	{
+		"name": "Austin TX",
+		"lat": 30.267,
+		"lng": -97.743,
+		"description": "Rooster Teeth Expo," + " July 7th - 9th," + " http://www.rtxaustin.com/"
+	},
+	{
+		"name":"San Jose CA",
+		"lat": 37.338,
+		"lng": -121.886,
+		"description": "Realities 360 Conference, " + " July 26th - 28th, " + "https://www.elearningguild.com/realities360/content/4900/2017-realities360-conference--home/?utm_campaign=r17se&utm_medium=social&utm_source=linkedin-arvrvw"
+	},
+	{
+		"name":"New York NY",
+		"lat": 40.712,
+		"lng": -74.005,
+		"description": "Games for Change, " + "July 31st - August 2nd, " + "http://festival.gamesforchange.org/"
+	},
+	{
+		"name":"Orlando FL",
+		"lat": 28.538,
+		"lng": -81.379,
+		"description": "Orlando Interactive Experience, " + "August 27th - August 30th, " + "http://www.orlandoix.com/"
+	},
+	{
+		"name":"Fiarfax, VA",
+		"lat": 38.846,
+		"lng": -77.306,
+		"description": "Serious Play Conferece, " + "July 18th-20th, " + "http://seriousplayconf.com/"
+	},
+	{
+		"name": "San Francisco CA",
+		"lat": 37.929,
+		"lng": -122.430,
+		"description": "VR Connects San Francisco, " + "June 27th-28th, " + "http://www.vr-connects.com/sanfrancisco/"
+	},
+	{   
+		"name":"Memphis TN",
+		"lat": 35.152,
+		"lng": -90.052,
+		"description": "Mid-South Indie Game Expo, " + "July 22nd, " + "http://www.midsouthigx.com/"
+	}
+	];
+
+
+	var newArray = ["i","o",2,3,4,5,6,7,8,9];
+
+	for (city in cities){
+		// console.log(cities[city]["name"]);    
+		var popup = new mapboxgl.Popup({offset: 50})
+		.setText(cities[city]["description"]);
+		var el = document.createElement('div');
+		el.className = 'marker';
+		new mapboxgl.Marker(el, {offset:[-25, -50]})
+		.setLngLat([cities[city]["lng"], cities[city]["lat"]])
+        .setPopup(popup) // sets a popup on this marker
+        .addTo(map);
+
+    }
+
+    $('#calendar').fullCalendar({
+    	defaultDate: '2017-06-12',
+    	editable: false,
+            eventLimit: true, // allow "more" link when too many events
+            events: [
+            {
+            	title: 'Terminus Conference',
+            	start: '2017-06-22',
+            	end: '2017-06-25',
+            	url: 'https://terminusevent.com/'
+            },
+            {
+            	title: '2D Con',
+            	start: '2017-08-11',
+            	end: '2017-08-13',
+            	url: 'https://www.2dcon.net/'
+            },
+            {
+            	title: 'Gen Con 50',
+            	start: '2017-08-17',
+            	end: '2017-08-20',
+            	url: 'http://www.gencon.com/'
+            },
+            {
+            	title: 'Rooster Teeth Expo',
+            	start: '2017-07-07',
+            	end: '2017-07-09',
+            	url: 'http://www.rtxaustin.com/'
+            },
+            {
+            	title: 'Realities 360 Conference',
+            	start: '2017-07-26',
+            	end: '2017-07-28',
+            	url: 'https://www.elearningguild.com/realities360/content/4900/2017-realities360-conference--home/?utm_campaign=r17se&utm_medium=social&utm_source=linkedin-arvrvw'
+            },
+            {
+            	title: 'Games for Change',
+            	start: '2017-07-31',
+            	end: '2017-08-02',
+            	url: 'http://festival.gamesforchange.org/'
+            },
+            {
+            	title: 'Orlando Interactive Experience',
+            	start: '2017-08-27',
+            	end: '2017-08-30',
+            	url: 'http://www.orlandoix.com/'
+            },
+            {
+            	title: 'Serious Play Conferece',
+            	start: '2017-07-18',
+            	end: '2017-07-20',
+            	url: 'http://seriousplayconf.com/'
+            },
+            {
+            	title: 'VR Connects San Francisco',
+            	start: '2017-06-27',
+            	end: '2017-06-28',
+            	url: 'http://www.vr-connects.com/sanfrancisco/'
+            },
+            {
+            	title: 'Mid-South Indie Game Expo',
+            	start: '2017-07-23',
+            	end: '2017-07-25',
+            	url: 'http://www.midsouthigx.com/'
+            }
+            ]
+        });
+$('.mapboxgl-canvas')[0].style.position = 'relative';
+$('.fc-event-container a').attr('target', "_blank" );
+
+}
 
 
 //Startup
